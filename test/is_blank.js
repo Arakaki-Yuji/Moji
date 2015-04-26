b8fa465d30f911cb5117c1ded1da5ec1d41fe888
Arakaki-Yuji <arakaki@ryukyu-i.co.jp>
2015-04-27 08:57:32 +0900

    move test for camelize method to test/camelize.js

--- test/is_blank.js
+++ test/is_blank.js
@@ -26,23 +26,4 @@ describe('Moji', function () {
             assert.equal(str, "Hello World");
         });
     });
-
-    describe('cameraize', function(){
-        it('if second argument is null, return UpperCamel of string', function(){
-            assert.equal(Moji.camelize('name'), 'Name');
-            assert.equal(Moji.camelize('name_snake'), 'NameSnake');
-        });
-        it('if second argument is `upper`, return UpperCamel of string', function(){
-            assert.equal(Moji.camelize('name', 'upper'), 'Name');
-            assert.equal(Moji.camelize('name_snake', 'upper'), 'NameSnake');
-        });
-        it('if second argument is `lower`, return LowerCamel of string', function(){
-            assert.equal(Moji.camelize('name', 'lower'), 'name');
-            assert.equal(Moji.camelize('name_snake', 'lower'), 'nameSnake');
-        });
-        if('if second argument is not `lower` or `upper`, throw Error', function(){
-            assert.throw(Moji.camelize('name', 'not_format'),
-                         'Second argument must be `upper` or `lower`');
-        });
-    });
 });
