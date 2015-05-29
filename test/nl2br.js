38a5e7cade0be13448f7b2cc0e9ebcaa2766787f
Arakaki-Yuji <arakaki@ryukyu-i.co.jp>
2015-05-29 16:27:29 +0900

    define nl2br method

---
+++ test/nl2br.js
@@ -0,0 +1,15 @@
+var assert = require('assert');
+var Moji = require('../lib/index.js');
+
+describe('Moji', function () {
+    describe('nl2br', function(){
+        it('replace newline character to <br />', function(){
+            assert.equal(Moji.nl2br('Hello\nWorld'), 'Hello<br />World');
+            assert.equal(Moji.nl2br('Hello\rWorld'), 'Hello<br />World');
+            assert.equal(Moji.nl2br('Hello\r\nWorld'), 'Hello<br />World');
+            moji = "Hello\
+World";
+            assert.equal(Moji.nl2br(moji, 'Hello<br />World'));
+        });
+    });
+});
